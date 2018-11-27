41af2e08a51e7d79c7617014ecbb4c70a30052aa
Dendi Handian <dhn@voxteneo.asia>
2018-11-27 11:49:09 +0700

    Updates

---
+++ precache-manifest.79e409ff5f7955a0f885c1ff43587a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-address-book/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6844d9f2bbc623430f9",
+    "url": "/react-address-book/static/js/runtime~main.c6844d9f.js"
+  },
+  {
+    "revision": "5d73dc674576d319fc1e",
+    "url": "/react-address-book/static/js/main.5d73dc67.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-address-book/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "5d73dc674576d319fc1e",
+    "url": "/react-address-book/static/css/main.1e922d6d.chunk.css"
+  },
+  {
+    "revision": "709679d2dd3d14fa3cd0e068f1820397",
+    "url": "/react-address-book/index.html"
+  }
+];
